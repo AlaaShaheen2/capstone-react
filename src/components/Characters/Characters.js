f757b98dde4d1e2f0df22832e97107c6a080bf04
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2022-10-21 16:38:00 +0430

    Refactore characters

--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-const Characters = () => (
-  <h1>All Characters</h1>
-);
+const Characters = () => {
+  const displayChars = useSelector((state) => state.characters);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // if (!displayChars.length) dispatch(getData());
+  });
+
+  const [searchItem, setSearchItem] = useState('');
+
+  const handleSearch = (e) => {
+    if (e.searchItem === '' || e.searchItem === ' ') {
+      // dispatch(getData().sort());
+    }
+    // dispatch(charsFilter(searchItem));
+    setSearchItem(e.target.value);
+  };
+
+  return (
+    <>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Filter..."
+          onChange={handleSearch}
+          value={searchItem}
+        />
+      </div>
+
+      <div className="container">
+        {displayChars.sort().map((character, index) => (
+          <div className={(index % 2 === 0) ? 'row-bg card' : 'card'} key={character.key}>
+            <Link to={{ pathname: `/${character.id}` }} className="details-link">
+              <img className="character-img" src={character.image} alt={character.name} />
+              {' '}
+              <h3 className="character-name">
+                {(character.name[0] === '.' || character.name[0] === "'")
+                  ? character.name.replace(/[.']/g, '')
+                  : character.name}
+              </h3>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Characters;
